0762bdb791e26fddb2c07d41f1a9a1f7330c4dc5
lancasjo <104534650+lancasjo@users.noreply.github.com>
2023-05-20 21:01:05 -0400

    improved logic behind contact div

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const messageSend = document.getElementById("send-icon");
 const inputField = document.getElementById("message-box");
+const nameInput = document.getElementById("input-name");
+
 
 messageSend.addEventListener("click", function(){
     console.log(inputField.value);
     inputField.value = "";
+    nameInput.value = "";
+    alert("Message sent successfully!");
 })
